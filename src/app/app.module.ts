31efb0469348e365659ec6fd2efd0e304f0fff01
Matias Camiletti <matias.camiletti@gmail.com>
2019-09-25 11:25:55 -0300

    Mejorado funcionalidad del header para aceptar contenido.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { LayoutComponent } from './pages/layout/layout.component';
 import { fuseConfig } from './pages/layout/fuse-config';
 import { TestComponent } from './pages/test/test.component';
 import { TestTwoComponent } from './pages/test-two/test-two.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,10 @@ import { TestTwoComponent } from './pages/test-two/test-two.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    LayoutFuseModule.forRoot(fuseConfig)
+    LayoutFuseModule.forRoot(fuseConfig),
+
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
