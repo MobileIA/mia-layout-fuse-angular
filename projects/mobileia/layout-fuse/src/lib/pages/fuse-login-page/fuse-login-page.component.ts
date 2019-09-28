93b80f89a8f92f258ae5aa0d12e66f5fb0275e87
Matias Camiletti <matias.camiletti@gmail.com>
2019-09-28 12:24:54 -0300

    Mejorado el manejo del Splash y Loading.

--- projects/mobileia/layout-fuse/src/lib/pages/fuse-login-page/fuse-login-page.component.ts
+++ projects/mobileia/layout-fuse/src/lib/pages/fuse-login-page/fuse-login-page.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { fuseAnimations } from '../../animations';
 import { FuseLoginConfig } from '../../types/fuse-login-config';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from '@mobileia/authentication';
 import { switchMap, map } from 'rxjs/operators';
+import { DOCUMENT } from '@angular/common';
+import { AnimationPlayer, AnimationBuilder, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'mia-fuse-login-page',
@@ -15,17 +17,23 @@ import { switchMap, map } from 'rxjs/operators';
 })
 export class FuseLoginPageComponent implements OnInit {
 
+  splashScreenEl: any;
+  player: AnimationPlayer;
+
   loginForm: FormGroup;
   config: FuseLoginConfig;
-  isLoading = true;
   loginMessageError = '';
 
   constructor(
+    @Inject(DOCUMENT) private document: any,
+    private animationBuilder: AnimationBuilder,
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
     private authService: AuthenticationService
   ) {
+    // Get the splash screen element
+    this.splashScreenEl = this.document.body.querySelector('#fuse-splash-screen');
     this.buildForm();
   }
 
@@ -68,18 +76,24 @@ export class FuseLoginPageComponent implements OnInit {
     }))
     .pipe(switchMap(params => {
       const redirect = params.redirect;
-      if (redirect !== '/login' && redirect !== '/' && redirect !== '' && redirect !== null 
+      if (redirect !== '/login' && redirect !== '/' && redirect !== '' && redirect !== null
       && redirect !== undefined && redirect !== '%2F' && redirect !== '/login;redirect=%2F') {
         this.config.successRoute = redirect;
       }
 
       return this.authService.isLoggedIn;
     }))
-    .subscribe(isLogged => {
+    .pipe(switchMap(isLogged => {
       if (isLogged) {
+        this.hideSplash();
         this.router.navigateByUrl(this.config.successRoute);
       }
-      this.isLoading = false;
+      return this.authService.getAccessToken();
+    }))
+    .subscribe(accessToken => {
+      if (accessToken == null || accessToken == '' || accessToken == undefined) {
+        this.hideSplash();
+      }
     });
   }
 
@@ -103,5 +117,43 @@ export class FuseLoginPageComponent implements OnInit {
       return true;
     }
     return false;
+  }
+
+  /**
+   * Show the splash screen
+   */
+  showSplash(): void {
+    this.player =
+        this.animationBuilder
+            .build([
+                style({
+                    opacity: '0',
+                    zIndex : '99999'
+                }),
+                animate('400ms ease', style({opacity: '1'}))
+            ]).create(this.splashScreenEl);
+
+    setTimeout(() => {
+        this.player.play();
+    }, 0);
+  }
+
+  /**
+   * Hide the splash screen
+   */
+  hideSplash(): void {
+      this.player =
+          this.animationBuilder
+              .build([
+                  style({opacity: '1'}),
+                  animate('400ms ease', style({
+                      opacity: '0',
+                      zIndex : '-10'
+                  }))
+              ]).create(this.splashScreenEl);
+
+      setTimeout(() => {
+          this.player.play();
+      }, 0);
   }
 }
