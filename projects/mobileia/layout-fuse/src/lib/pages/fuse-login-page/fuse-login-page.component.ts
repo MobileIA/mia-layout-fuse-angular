55eecd61b0c0f24608241311772d4bc9884ccae2
Matias Camiletti <matias.camiletti@gmail.com>
2019-09-28 12:04:42 -0300

    Agregada funcionalidad de la pantalla de login.

--- projects/mobileia/layout-fuse/src/lib/pages/fuse-login-page/fuse-login-page.component.ts
+++ projects/mobileia/layout-fuse/src/lib/pages/fuse-login-page/fuse-login-page.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { fuseAnimations } from '../../animations';
 import { FuseLoginConfig } from '../../types/fuse-login-config';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from '@mobileia/authentication';
+import { switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'mia-fuse-login-page',
@@ -15,11 +17,14 @@ export class FuseLoginPageComponent implements OnInit {
 
   loginForm: FormGroup;
   config: FuseLoginConfig;
+  isLoading = true;
+  loginMessageError = '';
 
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private authService: AuthenticationService
   ) {
     this.buildForm();
   }
@@ -29,14 +34,52 @@ export class FuseLoginPageComponent implements OnInit {
   }
 
   onSubmit() {
-    this.router.navigateByUrl(this.config.successRoute);
+    const email = this.loginForm.get('email').value;
+    const password = this.loginForm.get('password').value;
+    this.requestLogin(email, password);
+  }
+
+  requestLogin(email: string, password: string) {
+    // Limpiar mensaje de error
+    this.loginMessageError = '';
+    this.authService.signInWithEmailAndPassword(email, password).toPromise().then(data => {
+      if (data.success) {
+
+        if (this.isValidRole(data.response.role)) {
+          this.router.navigateByUrl(this.config.successRoute);
+        } else {
+          this.loginMessageError = 'Usted no tiene permisos para registrarse';
+          this.authService.signOut();
+        }
+
+      } else {
+        this.loginMessageError = data.error.message;
+      }
+    });
   }
 
   processConfig() {
-    this.route.data.subscribe(params => {
+    this.route.data
+    .pipe(switchMap(params => {
       if (params.config) {
         this.config = params.config;
       }
+      return this.route.queryParams;
+    }))
+    .pipe(switchMap(params => {
+      const redirect = params.redirect;
+      if (redirect !== '/login' && redirect !== '/' && redirect !== '' && redirect !== null 
+      && redirect !== undefined && redirect !== '%2F' && redirect !== '/login;redirect=%2F') {
+        this.config.successRoute = redirect;
+      }
+
+      return this.authService.isLoggedIn;
+    }))
+    .subscribe(isLogged => {
+      if (isLogged) {
+        this.router.navigateByUrl(this.config.successRoute);
+      }
+      this.isLoading = false;
     });
   }
 
@@ -45,5 +88,20 @@ export class FuseLoginPageComponent implements OnInit {
       email: ['', Validators.compose([Validators.required, Validators.email]) ],
       password: ['', Validators.compose([Validators.required, Validators.minLength(6)]) ],
     });
+  }
+
+  /**
+   * Funcion que determinar si el usuario para ingresar necesita un rol especial
+   */
+  isValidRole(userRole: number): boolean {
+    // Si no necesita verificar rol
+    if (!this.config.verifyRole) {
+      return true;
+    }
+    // Verificar si es el mismo rol
+    if (this.config.withRole == userRole) {
+      return true;
+    }
+    return false;
   }
 }
